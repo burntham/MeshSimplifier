4aa5f9cc80b9134bb1a7b8163ee68d9a5b39b1c3
burntham <drbking@gmail.com>
2014-07-24 10:25:16 +0200

    Initial Commit

---
+++ SimplFy/src/SimplFy.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+//io
+#include <vcg/complex/complex.h>
+#include <wrap/io_trimesh/import.h>
+#include <wrap/io_trimesh/export_ply.h>
+
+#include <vcg/complex/algorithms/local_optimization.h>
+#include <vcg/complex/algorithms/local_optimization/tri_edge_collapse_quadric.h>
+
+using namespace vcg;
+using namespace tri;
+
+//Class prototypes for Mesh definition
+class MyVertex;
+class MyEdge;
+class MyFace;
+
+/*NB templated struct?*/
+struct MyUsedTypes: public UsedTypes<Use<MyVertex>::AsVertexType, Use<MyEdge>::AsEdgeType,
+		Use<MyFace>::AsFaceType>{};
+
+class MyVertex: public Vertex<MyUsedTypes, vertex::VFAdj, vertex>
+
+
+int main()
+{
+	using namespace std;
+	std::cout<<"Hello World!"<<"\n";
+	return 5;
+}
