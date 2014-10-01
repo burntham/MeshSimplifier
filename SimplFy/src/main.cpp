d04bb80f4cd4cbeae5512b6c6df100c11f230920
Daniel Burnham-King <drbking@gmail.com>
2014-10-01 14:09:10 +0200

    BB TEST

--- SimplFy/src/main.cpp
+++ SimplFy/src/main.cpp
@@ -57,11 +57,11 @@ int main(int argc, char**argv)
 
     Point3f min = Mesh.bbox.min;
     Point3f max = Mesh.bbox.max;
-    min.X()+=100;
-    min.Y()+=100;
+    min.X()+=5000;
+    min.Y()+=5000;
     min.Z()+=100;
-    max.X()-=100;
-    max.Y()-=100;
+    max.X()-=5000;
+    max.Y()-=5000;
     max.Z()-=100;
 
     Mesh.workingBBox = Box3f(min,max);
